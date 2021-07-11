8205ff01c9e4f60a59fbc1d54fd3fa939d1b3671
Ant Viv <antvivaldi@protonmail.com>
2021-07-11 20:06:44 +0200

    Regex

---
+++ Workflow/src/RegexExample.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("[abc]*");
+        Matcher m = p.matcher("ababa");
+        boolean b = m.matches();
+
+        boolean b1 = Pattern.compile(".as.").matcher("cast").matches();
+
+        boolean b2  = Pattern.matches("[ap]", "p");
+
+        System.out.println("Hi");
+        System.out.println(b + " " + b1 + " " + b2);
+    }
+}
+
