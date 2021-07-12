345594e7c7cd29ce8266fe73ad33192dd4a43928
Ant Viv <antvivaldi@protonmail.com>
2021-07-12 12:54:45 +0200

    Da

--- Workflow/src/RegexExample.java
+++ Workflow/src/RegexExample.java
@@ -4,12 +4,12 @@ import java.util.regex.Pattern;
 public class RegexExample {
     public static void main(String[] args) {
         Pattern p = Pattern.compile("[abc]*");
-        Matcher m = p.matcher("Good");
+        Matcher m = p.matcher("Da");
         boolean b = m.matches();
 
         boolean b1 = Pattern.compile(".as.").matcher("cast").matches();
 
-        boolean b2  = Pattern.matches("[ap]", "p");
+        boolean b2  = Pattern.matches("[hi]", "p");
 
         System.out.println("Hi");
         System.out.println(b + " " + b1 + " " + b2);
